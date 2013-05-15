facb497da8e520b2f4b14ff8a40395fa1fa51af4
bjo <aziznote2@gmail.com>
2013-05-15 08:01:40 +0700

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstRubyonrails::Application.config.secret_token = 'b0d63fef4fb12ed09f9d124e03186f86579e87bbe73bc89eee145eaa59144b107704689e6b6547e54f2cd1945e56f1a2712fe1d4245f1f3cd8ce89199372a1cc'
